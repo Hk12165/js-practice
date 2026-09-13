78cab42295cb81463329d9b82c423dd309b93ee7
Hk12165 <harsh88780@gmail.com>
2026-09-13 17:00:53 +0530

    init js practice

---
+++ js-basics.js
@@ -0,0 +1,28 @@
+const name = "Harsh";
+const age = 19;
+const isStudent = true;
+const city = "Mumbai";
+const favLang = "JavaScript";
+const yearsOfCoding = 2;
+const codingStack = ["HTML", "CSS", "JavaScript"];
+
+// reassigning the value of age
+// age = 21;
+// error: TypeError: Assignment to constant variable.
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(city);
+console.log(favLang);
+console.log(yearsOfCoding);
+console.log(codingStack);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof city);
+console.log(typeof favLang);
+console.log(typeof yearsOfCoding);
+console.log(typeof codingStack);
+
